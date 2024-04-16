52e1f4b9f1e7aae67558e51bf69b30ab0393bc34
PrajaktaBhagwat2001 <100439224+PrajaktaBhagwat2001@users.noreply.github.com>
2024-04-16 14:38:35 +0530

    Add files via upload

---
+++ lab.sql
@@ -0,0 +1,50 @@
+drop database laibrary;
+create database laibrary;
+use laibrary;
+CREATE TABLE Staff(
+    Staff_Id INT PRIMARY KEY,
+    s_name varchar(20)
+    );
+
+CREATE TABLE Book_Details (
+    book_id INT PRIMARY KEY,
+    book_author VARCHAR(255),
+    book_name VARCHAR(255),
+    book_price DECIMAL(10, 2),
+    place_number INT,
+    status VARCHAR(50),
+    edition VARCHAR(50),
+    date_purchage DATE,
+    number_copies INT
+);
+-- Create the Student Details table
+CREATE TABLE Student_Details (
+    id INT PRIMARY KEY,
+    full_name VARCHAR(255),
+    university_id VARCHAR(50),
+    student_address VARCHAR(255),
+    admission_date DATE,
+    max_book_limit INT,
+    no_books_issued INT,
+    academic_year VARCHAR(50)
+);
+
+-- Create the Transaction History table
+DROP TABLE Transaction_History;
+CREATE TABLE Transaction_History (
+    transaction_id INT PRIMARY KEY,
+    member_id INT,
+    book_id INT,
+    date_issue DATE,
+    due_date DATE,
+    returned_date DATE,
+    fine_collected DECIMAL(10, 2),
+    transaction_status VARCHAR(50),
+   --  FOREIGN KEY (book_id) REFERENCES Book_Details(book_id),
+--     FOREIGN KEY (id) REFERENCES Student_Details(id)
+--     
+id INT,
+book_id INT,
+     FOREIGN KEY (id) REFERENCES Student_Details(id),
+    FOREIGN KEY (book_id) REFERENCES Book_Details(book_id)
+);
